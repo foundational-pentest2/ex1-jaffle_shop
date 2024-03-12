b8f15f1629934d4a24a8a93110b714b3a668d625
orendar <Oren@foundational.io>
2024-03-12 14:48:00 +0200

    foundational-demo:use-customer-address-to-define-billing-and-shipping-address-in-stg-tables

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -4,19 +4,29 @@ with source as (
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ ref('raw_orders') }}
+select * from {{ ref('raw_orders') }}
 
-),
+    ),
+
+customer_address as (
+    select
+        id,
+        address,
+    from  {{ ref('raw_customers') }}
+)
 
 renamed as (
 
     select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+        source.id as order_id,
+        source.user_id as customer_id,
+        source.order_date,
+        source.status
+        customer_address.address as shipping_address
 
     from source
+    join customer_address
+    on source.user_id = customer_address.id
 
 )
 
