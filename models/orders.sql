26912c628a481e3f3fb8b41d8efddb29d38fa7d2
orendar <Oren@foundational.io>
2024-03-13 09:55:50 +0200

    use-customer-address-to-define-billing-and-shipping-address-in-stg-tables

--- models/orders.sql
+++ models/orders.sql
@@ -36,7 +36,7 @@ final as (
         orders.customer_id,
         orders.order_date,
         orders.status,
-
+        orders.shipping_address,
         {% for payment_method in payment_methods -%}
 
         order_payments.{{ payment_method }}_amount,
