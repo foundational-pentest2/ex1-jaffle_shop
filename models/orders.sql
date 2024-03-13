4261e97fdf06e7b2624f55c737bd44351d0e44a0
orendar <Oren@foundational.io>
2024-03-13 17:40:43 +0200

    rename address to default_shipping_address

--- models/orders.sql
+++ models/orders.sql
@@ -52,10 +52,10 @@ final as (
 
     from orders
     left join order_payments
-        on ( orders.order_id = order_payments.order_id )
+        on orders.order_id = order_payments.order_id
 
     left join customers
-        on ( orders.customer_id = customers.customer_id )
+        on orders.customer_id = customers.customer_id
 )
 
 select * from final
