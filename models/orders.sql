4c8d8029f26ec992c4992a90a6185134ff8f6369
orendar <Oren@foundational.io>
2024-03-13 16:30:50 +0200

    test

--- models/orders.sql
+++ models/orders.sql
@@ -36,7 +36,7 @@ final as (
         orders.customer_id,
         orders.order_date,
         orders.status,
-        orders.address as shipping_address,
+        customers.address as shipping_address,
 
         {% for payment_method in payment_methods -%}
 
@@ -51,6 +51,9 @@ final as (
 
     left join order_payments
         on orders.order_id = order_payments.order_id
+
+    left join customers
+        on orders.customer_id = customers.customer_id
 
 )
 
