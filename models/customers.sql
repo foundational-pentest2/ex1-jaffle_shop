d8c2ee8bff12b9781f0f08cd9a8298ff9cef8a8c
Just-Special-Test <just.special.test@gmail.com>
2025-04-15 12:47:16 +0300

    Update customers.sql

--- models/customers.sql
+++ models/customers.sql
@@ -55,7 +55,8 @@ final as (
         customer_orders.first_order,
         customer_orders.most_recent_order,
         customer_orders.number_of_orders,
-        customer_payments.total_amount as customer_lifetime_value
+        customer_payments.total_amount as customer_lifetime_value,
+        customer_payments.total_amount as customer_lifetime_value_test2
 
     from customers
 
